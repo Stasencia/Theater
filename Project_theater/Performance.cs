c812f2e984f2ce2848418a7e36bf4c04c036c45a
Stasencia <44728575+Stasencia@users.noreply.github.com>
2018-12-16 18:27:41 +0200

    Project version 2.2

--- Project_theater/Performance.cs
+++ Project_theater/Performance.cs
@@ -25,24 +25,52 @@ namespace Project_theater
             this.Month_id = Month_id;
         }
 
-        private void Button_customization()
+        private async void Button_customization(object sender, EventArgs args)
         {
-            DateTime d = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
-            while( d.DayOfWeek != DayOfWeek.Monday)
+            string[] words = ((Control)sender).Tag.ToString().Split(';');
+            DateTime d = new DateTime(Convert.ToInt32(words[1]), Convert.ToInt32(words[0]), 1);
+            while (d.DayOfWeek != DayOfWeek.Monday)
             {
                 d = d.AddDays(-1);
             }
-            for(int i = 0;i<42;i++)
+            for (int i = 0;i<42;i++)
             {
-               Controls["b" + (i+1)].Text = d.Day.ToString();
-                if (d.Month != DateTime.Today.Month)
-                    Controls["b" + (i + 1)].Enabled = false;
-               d = d.AddDays(1);
+                Controls["b" + (i + 1)].Text = d.Day.ToString();
+                Controls["b" + (i + 1)].Enabled = false;
+                Controls["b" + (i + 1)].BackgroundImage = null;
+                Controls["b" + (i + 1)].Tag = d.Month + ";" + d.Year;
+                d = d.AddDays(1);
             }
+            using (SqlConnection connection = new SqlConnection(DB_connection.connectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand("SELECT Id_performance, Date, DAY(Date) AS day, Afisha.Small_image FROM [Afisha_dates] LEFT JOIN[Afisha] ON Afisha_dates.Id_performance = Afisha.Id WHERE Id_performance = @Id AND MONTH(Date) = @month AND YEAR(Date) = @year", connection);
+                command.Parameters.AddWithValue("@Id", perf_id);
+                command.Parameters.AddWithValue("@month", Convert.ToInt32(words[0]));
+                command.Parameters.AddWithValue("@year", Convert.ToInt32(words[1]));
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        for (int i = 0; i < 42; i++)
+                        {
+                            if (Controls["b" + (i + 1)].Text == reader.GetValue(2).ToString() && (Convert.ToInt32(Controls["b" + (i + 1)].Text) >= DateTime.Now.Day || Convert.ToInt32(Controls["b" + (i + 1)].Tag.ToString().Split(';')[1]) > DateTime.Now.Year) && Controls["b" + (i + 1)].Tag.ToString().StartsWith(words[0]))
+                            {
+                                string s = DB_connection.current_directory + "images_afisha\\" + reader.GetValue(3).ToString();
+                                Controls["b" + (i + 1)].Enabled = true;
+                                Controls["b" + (i + 1)].BackgroundImage = new Bitmap(@s);
+                            }
+                        }
+                    }
+                } 
+            }
+            
         }
 
         private async void Performance_Load(object sender, EventArgs e)
         {
+            Button push = new Button();
             AutoSize = false;
             Size = new Size(797, 530);
             using (SqlConnection connection = new SqlConnection(DB_connection.connectionString))
@@ -63,12 +91,47 @@ namespace Project_theater
                 }
             }
             label9.Height = (int)(((label9.Text.Length * label9.Font.SizeInPoints) / label9.Width + label9.Text.Where(x=>x == '\n').Count()) * label9.Font.Height);
+            using (SqlConnection connection = new SqlConnection(DB_connection.connectionString))
+            {
+                await connection.OpenAsync();
+                int i = 0;
+                Months m;
+                SqlCommand command2 = new SqlCommand("SELECT DISTINCT MONTH(Date), YEAR(Date) FROM Afisha_dates WHERE Id_performance = @Id AND ((MONTH(Date) >= MONTH(GETDATE()) AND YEAR(Date) >= YEAR(GETDATE())) OR YEAR(Date) > YEAR(GETDATE())) ORDER BY YEAR(Date)", connection);
+                command2.Parameters.AddWithValue("@Id", perf_id);
+                SqlDataReader reader2 = command2.ExecuteReader();
+                if (reader2.HasRows)
+                {
+                    while (reader2.Read())
+                    {
+                        Button top = new Button();
+                        top.FlatStyle = FlatStyle.Flat;
+                        m = (Months)reader2.GetInt32(0);
+                        top.Text = m + "\n" + reader2.GetValue(1).ToString();
+                        top.AutoSize = false;
+                        top.Size = new Size(82, 49);
+                        top.Font = new Font("Century Gothic", 10, FontStyle.Regular);
+                        top.Tag = reader2.GetValue(0) + ";" + reader2.GetValue(1);
+                        if (Convert.ToInt32(reader2.GetValue(0)) == Month_id)
+                            push = top;
+                        top.BringToFront();
+                        top.Click += new System.EventHandler(this.Button_customization);
+                        top.Name = "top" + (i + 1);
+                        Controls.Add(top);
+                        i++;
+                    }
+                }
+                int k = (797 - (82 * i + (i - 1) * 22)) / 2;
+                for (int j = 0; j < i; j++)
+                {
+                    Controls["top" + (j + 1)].Location = new Point(k + j * 82 + j * 22, label9.Bottom);
+                }
+            }
             Button[] b = new Button[42];
             for (int i = 0; i < 42; i++)
             {
                 b[i] = new Button();
                 b[i].Size = new Size(90, 90);
-                b[i].Location = new Point(75 + (89 * (i % 7)), label9.Bottom + (89 * (int)Math.Floor(i / 7.0)));
+                b[i].Location = new Point(75 + (89 * (i % 7)), Controls["top1"].Bottom + 15 + (89 * (int)Math.Floor(i / 7.0)));
                 b[i].FlatStyle = FlatStyle.Flat;
                 b[i].TextAlign = ContentAlignment.TopLeft;
                 b[i].BackgroundImageLayout = ImageLayout.Stretch;
@@ -76,9 +139,11 @@ namespace Project_theater
                 b[i].MouseLeave += new EventHandler(button_MouseLeave);
                 b[i].Font = new Font("Century Gothic", 9, FontStyle.Regular);
                 b[i].Name = "b" + (i + 1);
+                b[i].Click += new System.EventHandler(this.Day_pushed);
                 Controls.Add(b[i]);
             }
-            Button_customization();
+            //push.Select();
+            push.PerformClick();
             panel2.Size = new Size(15, 15);
             panel2.Location = new Point(0, b[41].Bottom);
             panel2.BringToFront();
@@ -89,16 +154,26 @@ namespace Project_theater
             p.Height = 90;
             p.Width = 90;
             p.Location = new Point(p.Location.X + 10, p.Location.Y + 10);
-            p.BackgroundImage = null;
         }
 
-        private void button_MouseEnter(object sender, EventArgs e)
+        private async void button_MouseEnter(object sender, EventArgs e)
         {
             Button p = (Button)sender;
             p.Height = 110;
             p.Width = 110;
             p.Location = new Point(p.Location.X - 10, p.Location.Y - 10);
-            p.BackgroundImage = new Bitmap(@"C:\Users\Stasia\Desktop\uni_2018\Project_theater\Theater\Project_theater\Resources\111477-theatre.png");
+            using (SqlConnection connection = new SqlConnection(DB_connection.connectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand("SELECT * FROM [Afisha] WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", perf_id);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    string s = DB_connection.current_directory + "images_afisha\\" + reader.GetValue(11).ToString();
+                    p.BackgroundImage = new Bitmap(@s);
+                }
+            }
             p.BringToFront();
         }
 
@@ -115,6 +190,13 @@ namespace Project_theater
         private void Performance_FormClosing(object sender, FormClosingEventArgs e)
         {
             a.Show();
+        }
+
+        private void Day_pushed(object sender, EventArgs e)
+        {
+            Ticket_purchase t = new Ticket_purchase();
+            t.Show();
+            this.Hide();
         }
     }
 }
---
+++ Project_theater/Ticket_purchase.cs
@@ -0,0 +1,27 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_theater
+{
+    public partial class Ticket_purchase : MetroForm
+    {
+        public Ticket_purchase()
+        {
+            InitializeComponent();
+        }
+
+        private void Ticket_purchase_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MainForm main = new MainForm();
+            main.Show();
+        }
+    }
+}
